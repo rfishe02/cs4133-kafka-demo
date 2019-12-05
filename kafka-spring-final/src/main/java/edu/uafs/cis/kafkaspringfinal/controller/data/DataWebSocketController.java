31537f3399281be14cec9fcf402debeefe3c8b7d
Renae F <43683461+rfishe02@users.noreply.github.com>
2019-12-05 10:24:05 -0600

    Update DataWebSocketController.java

--- kafka-spring-final/src/main/java/edu/uafs/cis/kafkaspringfinal/controller/data/DataWebSocketController.java
+++ kafka-spring-final/src/main/java/edu/uafs/cis/kafkaspringfinal/controller/data/DataWebSocketController.java
@@ -14,32 +14,6 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class DataWebSocketController {
 
-  @Autowired 
-  private DataProducerService producer;
-  
-  /* 
-    This configuration sends data to a Kafka topic. 
-    A separate service will listen for changes to the topic.
-    
-    It's not necessary to use Kafka to relay messages. 
-    We can just use the annotation SendTo to relay data to a URL. 
-    
-    But, Kafka provides a pipeline that multiple services can read from.
-    If multiple services need to use data sent through the socket,
-    then it would be better to use Kafka to manage the data.
-    
-    @MessageMapping("/websocket-test")
-    @SendTo("/receive/websocket-test")
-    public WebSocketTestReceive testSocketResponse(WebSocketTestSend message) throws Exception {
-      return new UAMessage(message.getValue(),HtmlUtils.htmlEscape(message.getMessage()));
-    }
-    
-  */
-  
-  @MessageMapping("/websocket-data")
-  public void messageSocketResponse(WebSocketTestSend message) throws Exception {
-    this.producer.produceData(new Data(HtmlUtils.htmlEscape(message.getMessage()),message.getValue()));
-    
-  }
+  // Place your code here.
 
 }
